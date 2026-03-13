24501f22c8dfcc142d6b12cb399be4af069fe1d4
Andreskorevaar <110106611+Bluefire0781@users.noreply.github.com>
2026-03-13 13:10:06 +0100

    Mock unit test

--- Dock/api/src/api/mod.rs
+++ Dock/api/src/api/mod.rs
@@ -6,9 +6,25 @@ pub fn router() -> Router {
     Router::new()
         .route("/", axum::routing::get(root))
         .route("/users", axum::routing::post(users::create_user))
+        .route("/add", axum::routing::get(add))
 }
 
 // basic handler that responds with a static string
 async fn root() -> &'static str {
     "Hello, World!"
+}
+
+async fn add() -> String {
+    (4 + 4).to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_add() {
+        let result = add().await;
+        assert_eq!(result, "8");
+    }
 }
