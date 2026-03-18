d1de6171c8fe1d61d38ae5a60f28ac6b6e8a22cc
Andreskorevaar <110106611+Bluefire0781@users.noreply.github.com>
2026-03-18 16:15:28 +0100

    add models + seaorm to lock

---
+++ Dock/api/src/models/afspraak.rs
@@ -0,0 +1,30 @@
+use sea_orm::entity::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+pub struct CreateAfspraak {
+    pub rit_nmr: u64,
+    pub dock_nmr: u64,
+    pub starttijd: String,
+    pub eindtijd: String,
+    pub landing_type: String,
+}
+
+#[sea_orm::model]
+#[derive(Serialize, Debug, Clone, PartialEq, Eq, DeriveEntityModel, Default)]
+#[sea_orm(table_name = "afspraak")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub afspraak_id: u64, //pk
+    pub rit_nmr: u64,  //fk
+    pub dock_nmr: u16, //fk
+    pub starttijd: String,
+    pub eindtijd: String,
+    pub landing_type: String,
+    #[sea_orm(belongs_to, from = "rit_nmr", to = "rit_id")]
+    pub rit: HasOne<super::rit::Entity>,
+    #[sea_orm(belongs_to, from = "dock_nmr", to = "dock_id")]
+    pub dock: HasOne<super::dock::Entity>,
+}
+
+impl ActiveModelBehavior for ActiveModel {}
--- Dock/api/src/models/mod.rs
+++ Dock/api/src/models/mod.rs
@@ -1,3 +1,11 @@
+mod afspraak;
+mod dock;
+mod leverancier;
+mod rit;
 mod user;
 
+pub use afspraak::*;
+pub use dock::*;
+pub use leverancier::*;
+pub use rit::*;
 pub use user::*;
