3dbdace3a9f04a177931ae18d05acdb458fbd73c
Andreskorevaar <110106611+Bluefire0781@users.noreply.github.com>
2026-03-25 13:34:03 +0100

    update

--- Dock/api/src/service/leverancier_service.rs
+++ Dock/api/src/service/leverancier_service.rs
@@ -5,18 +5,19 @@ use argon2::{
 };
 use sea_orm::{ActiveModelTrait, DatabaseConnection, Set};
 
-// Helper function to hash password
+// function to hash password
 fn hash_password(password: &str) -> Result<String, argon2::password_hash::Error> {
     let salt = SaltString::generate(&mut OsRng);
     let argon2 = Argon2::default();
 
     Ok(argon2
-        .hash_password(password.as_bytes(), &salt)? // ← Same as docs
-        .to_string()) // ← Same as docs
+        .hash_password(password.as_bytes(), &salt)?
+        .to_string())
 }
 
+// function to create leverancier
 pub async fn create_leverancier(
-    db: &DatabaseConnection, // Accept connection as parameter
+    db: &DatabaseConnection, // DB
     leverancier_nmr: u64,
     username: String,
     password: String,
@@ -24,17 +25,21 @@ pub async fn create_leverancier(
     ppu: u16,
     role: Option<String>,
 ) -> Result<leverancier::Model, sea_orm::DbErr> {
+    // creating hashed password
     let hashed_password =
         hash_password(&password).map_err(|e| sea_orm::DbErr::Custom(e.to_string()))?;
 
+    // creating leverancier class
     let new_leverancier = leverancier::ActiveModel {
         leverancier_id: Set(leverancier_nmr),
         username: Set(username),
         password: Set(hashed_password),
         email: Set(email),
         ppu: Set(ppu),
+        role: Set(role),
         ..Default::default()
     };
 
+    // inserting into db
     new_leverancier.insert(db).await // Use the passed connection
 }
